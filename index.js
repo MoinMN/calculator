e244a813594e541b58b02581fb3c1f1991b3bcc2
Moin MN <100254727+MoinMN@users.noreply.github.com>
2022-12-29 21:22:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+//Sound effect
+let errorSound = new Audio('error.mp3');
+let likeButton = new Audio('glass breaking.mp3');
+
+let result = document.getElementById('calculation');
+
+let calculate=(number)=>{
+    result.value += number;
+}
+
+let calculator=()=>{
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        errorSound.play();
+        alert("Enter Valid Expression");
+    }
+}
+
+let clr=()=>{
+    result.value = "";
+}
+
+let del=()=>{
+    result.value = result.value.slice(0, -1);
+}
+
+let openBracket=()=>{
+    result.value += '(';
+}
+
+let closeBracket=()=>{
+    result.value += ')';
+}
+
+//Like button
+let likes = document.getElementById('likes');
+let count = 0;
+
+let hitLike=()=>{
+    likes.value = ++count;
+    likeButton.play();
+}
